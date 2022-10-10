85ccf86c322bb9da6649782028ae220256080b5e
Will Angelis <willangelis@gmail.com>
2022-10-09 23:33:11 -0300

    add training filter method

--- filter.js
+++ filter.js
@@ -1,5 +1,7 @@
 const { numbers } = require('./data.js');
 // importing array numbers from data.js
+const { pessoas } = require('./data.js');
+// importing array pessoas from data.js
 
 const numberFiltered = numbers.filter((number) => {
   // the filter method loop in each number of array and
@@ -10,3 +12,7 @@ const numberFiltered = numbers.filter((number) => {
 
 console.log(numberFiltered);
 // showing numbers filtered
+
+const name = pessoas.filter((pessoa) => pessoa.name === 'Carlos');
+
+name.length > 0 ? console.log(name) : console.log('O nome não existe');
