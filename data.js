a3c3907fb25b2d282064524c9379c19bb6a8068f
Will Angelis <willangelis@gmail.com>
2022-10-09 23:24:16 -0300

    add arrays for manipulation

---
+++ data.js
@@ -0,0 +1,22 @@
+const pessoas = [
+  {
+    name: 'Will',
+    idade: 26,
+  },
+  {
+    name: 'Bolinha de Angelis',
+    idade: 20,
+  },
+  {
+    name: 'Rosana',
+    idade: 48,
+  },
+  {
+    name: 'Bia',
+    idade: 18,
+  },
+];
+
+const numbers = [0, 2, 5, 20, 40, 32, 45, 63, 78];
+
+module.exports = { pessoas, numbers };
